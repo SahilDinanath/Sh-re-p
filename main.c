7deb11a53c8f25ef9e1faba1aa36aa7cf8acb68f
Sahil Dinanath <sahildinanath@gmail.com>
2024-07-06 15:50:23 +0200

    added ability to select options

--- main.c
+++ main.c
@@ -2,14 +2,43 @@
 #include <stdio.h>
 #include <string.h>
 #define LINESIZE 4095
+void info() {
+
+  fprintf(stderr, "Please provide a regular expression and file name eg: "
+                  "./main \"ab*|c\" main.c \nRun ./main -h to list help");
+}
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Please provide a regular expression and file name eg: "
-                    "./main \"ab*|c\" main.c \n");
+  // settings
+  int lineNumbers = 0, caseInsensitive = 0;
+
+  if (argc == 1) {
+    info();
     return 1;
   }
 
-  char *regex = argv[1], *file = argv[2], line[LINESIZE];
+  if (argc > 5) {
+    fprintf(stderr, "Too many arguments.\n");
+    info();
+    return 1;
+  }
+
+  char *input[2], **tmp;
+  tmp = input;
+  for (int i = 1; i < argc; i++) {
+    if (!strcmp(argv[i], "-h")) {
+      printf("Options:\n-i : case insensitivity\n-c : line numbers\n");
+      return 0;
+    } else if (!strcmp(argv[i], "-c")) {
+      lineNumbers = 1;
+    } else if (!strcmp(argv[i], "-i")) {
+      caseInsensitive = 1;
+    } else {
+      // extract the regex, then extract the file path.
+      *tmp++ = argv[i];
+    }
+  }
+
+  char *regex = input[0], *file = input[1], line[LINESIZE];
   compileRegex(regex);
 
   FILE *fptr = fopen(file, "r"), *ptr;
@@ -34,7 +63,11 @@ int main(int argc, char *argv[]) {
     while (token != NULL) {
       // if word is matched then get then stop matching
       if (match(token)) {
-        printf("%d: %s", i + 1, line);
+        if (lineNumbers) {
+          printf("%d: %s", i + 1, line);
+        } else {
+          printf("%s", line);
+        }
         break;
       }
       // increment search through line
